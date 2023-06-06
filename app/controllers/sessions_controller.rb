1bbcc15d15ea71e52277f389663513767065e813
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-06-06 17:08:54 +0000

    updated login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,16 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def index
+  end
+
   def create
     @user = User.find_by({"username" => params["username"]})
     if @user 
-      if BCrypt::Pasword.new(@user["password"]) == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "You are now logged in."
-        redirect_to "/"
+        redirect_to "/places"
       else
         flash["notice"] = "Sorry, invalid username or password."
         redirect_to "/places"
